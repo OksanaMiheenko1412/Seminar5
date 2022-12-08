61d062ed445817fb7064eab07a3a4af3c5909601
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-08 13:07:18 +0700

    / add  commit task33

---
+++ task33/Program.cs
@@ -0,0 +1,29 @@
+﻿// task33 Задайте массив. Напишите программу, которая определяет, присутствует ли
+// заданное чисор в массиве. Например: 4; array[6,7,19,345,3]-> нет
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+string ReleaseArray(int[] array, int k)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == k)
+            return "yes";
+    }
+    return "no";
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.Write("Введите число, которое Вы хотите найти в массиве: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ReleaseArray(array, k));
+
