4a0e273a4d07cee88eec86a42d53ee88f848f07d
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-09 12:03:00 +0700

    / add  commit task32

---
+++ task32/Program.cs
@@ -0,0 +1,24 @@
+﻿// task32 программа замены элементов массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.[-4,-8,8,2]->[4,8,-8,-2] 
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+         
+    {
+           array[i] = array[i] * -1;
+    }
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
